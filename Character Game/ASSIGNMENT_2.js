5569da6a491d6105ca9c53bc40e732035556d0d3
vshanu <vshanu@outlook.com>
2018-07-22 19:11:49 -0400

    Added Assignment 1 and 2

---
+++ Character Game/ASSIGNMENT_2.js
@@ -0,0 +1,14 @@
+function myFunction() {
+   var input =  document.getElementById("left").value;
+
+ /*function myFunction1(){
+
+   var chars = document.getElementById("left").split('');
+    document.getElementById("ExOutput").innerHTML = chars; 
+
+    var s = document.getElementById("left");
+    for (var i = 0; i < s.length; i++) {
+    console.log(s.charAt(i));}*/
+
+    document.getElementById("ExOutput").innerHTML = "input";
+}
